9374dd14665e97c4313581a76085073b94d20dd2
Aprendizeco <nparracampillo@gmail.com>
2026-02-26 14:05:31 -0500

    feat: creacion proyecto java

---
+++ src/main/java/com/example/Back_Final_Analisis/BackFinalAnalisisApplication.java
@@ -0,0 +1,13 @@
+package com.example.Back_Final_Analisis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFinalAnalisisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFinalAnalisisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Back_Final_Analisis/BackFinalAnalisisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Back_Final_Analisis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFinalAnalisisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
